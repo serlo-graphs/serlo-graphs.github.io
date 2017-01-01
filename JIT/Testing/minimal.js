b2714d1e819ee4eac8ca2214e999e6583e717120
Photon89 <photon89@googlemail.com>
2017-01-01 13:59:14 +0100

    Aufräumen

--- JIT/Testing/minimal.js
+++
@@ -1,1 +0,0 @@
-function init(){document.getElementById('demo').innerHTML = '2+2=4';}
--- JIT/Testing/minimal_including_body.js
+++
@@ -1,4 +0,0 @@
-function init(){
-	document.write('<div id="demo"->2+2=5</div>');
-	document.getElementById('demo').innerHTML = '2+2=4';
-}
